25cb17cd3f75833e03dae4553bfb42d6c4ad1839
Ian Walston <ikwalston@comcast.net>
2018-12-14 09:56:30 -0500

    scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log("hello")
+console.log("hello");
+
+//HERES AN AJAX CALL 
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+// }
+
+//HERES HOW TO DYNAMICALLY CREATE AND FILL AN HTML ELEMENT
+// var div=$("<div>");
+// div.text("heres something to add to the new div");
+// $("#output").append(div);
+
+//HERES HOW TO MAKE AN ON CLICK FUNCTION
+// $("#user-button").on("click",function (){
+//     console.log(this)
+// })
+
+//HERES HOW TO GET INPUT FROM USER
+// var userInput = $("#user-form").val()
