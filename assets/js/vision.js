d755db6b8b1c2c7c61722262db4d5e1f193675e1
Ian Walston <ikwalston@comcast.net>
2018-12-14 20:44:38 -0500

    now it displays colors

--- assets/js/vision.js
+++ assets/js/vision.js
@@ -1,22 +1,19 @@
-//THIS DATA IS FOR USE WITH OTHER FEATURES OF GOOGLE VISION API 
-// const features = ['LABEL_DETECTION', 'TEXT_DETECTION', 'FACE_DETECTION', 'LANDMARK_DETECTION', 'LOGO_DETECTION', 'SAFE_SEARCH_DETECTION', 'IMAGE_PROPERTIES']
-// const likelyhoodValue = {
-//     'UNKNOW':null,
-// 'VERY_UNLIKELY':-2,
-// 'UNLIKELY':-1,
-// 'POSSIBLE':0, 
-// 'LIKELY':1,
-//  'VERY_LIKELY':2 }
 
-//defining function for the call to the Google Vision API
-var googleVisionCall = (url) => {
+const features = ['LABEL_DETECTION', 'TEXT_DETECTION', 'FACE_DETECTION', 'LANDMARK_DETECTION', 'LOGO_DETECTION', 'SAFE_SEARCH_DETECTION', 'IMAGE_PROPERTIES']
+const likelyhoodValue = {
+    'UNKNOW': null,
+    'VERY_UNLIKELY': -2,
+    'UNLIKELY': -1,
+    'POSSIBLE': 0,
+    'LIKELY': 1,
+    'VERY_LIKELY': 2
+}
 
-    //appending request image to display div
-    var img = $("<img>").attr("src",url)
-    $("#display").append(img)
-    
+//defining function to call google Vision, get image properties, store colors in local memory and display colors in html
+var googleVisionImageProperties = (url) => {
+
     //api request to google vision
-    var visionOption = JSON.stringify({ "requests": [{ "image": { "source": { "imageUri": url } }, "features": [{ "type": "IMAGE_PROPERTIES", "maxResults": 5 }] }] });
+    var visionOption = JSON.stringify({ "requests": [{ "image": { "source": { "imageUri": url } }, "features": [{ "type": 'IMAGE_PROPERTIES', "maxResults": 5 }] }] });
     var visionResponse = new XMLHttpRequest;
     visionResponse.open("POST", "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA9EHeI2lrYJnjfEMhI0rU-J8yyfAOSOAs", !0);
     visionResponse.send(visionOption)
@@ -31,20 +28,27 @@ var googleVisionCall = (url) => {
 
         //iterating over array of colors
         for (i = 0; i < colorsArray.length; i++) {
-            let color = colorsArray[i].color    
+            let color = colorsArray[i].color
 
-            $("#display").append(JSON.stringify(color))
-            console.log(color)
+            //reformat color from googleVision response to css 
+            let rgbColor = `rgb(${color.red},${color.green},${color.blue})`
+
+            //displaying css formatted color in the DOM
+            $("#display").append($("<div>").text(rgbColor).css(`background-color`,rgbColor).css('min-height','100px'))
 
             //storing each color from the array into local storage
-            localStorage.setItem(`newColor${i}`, JSON.stringify(color))
+            localStorage.setItem(`newColor${i}`, rgbColor)
         }
     }
 }
 
-//making the submit but run the googleVisionCall with the url from the userInput form
+//making the submit button call the googleVisionCall with the url from the userInput form, and display image
 $("#submitButton").on("click", function () {
-    var userInput = $("#userInput").val()
     event.preventDefault()
-    googleVisionCall(userInput)
+    var userInput = $("#userInput").val()
+    //appending request image to display div
+    var img = $("<img>").attr("src", userInput)
+    $("#display").append(img)
+    
+    googleVisionImageProperties(userInput)
 })
