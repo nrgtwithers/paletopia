f4f40a746978bbeb0144f5c5944e7a56586c296f
Ian Walston <ikwalston@comcast.net>
2018-12-17 18:52:57 -0500

    fixed issue with display in savebutton.js

--- assets/js/savebutton.js
+++ assets/js/savebutton.js
@@ -1,7 +1,7 @@
 $("#saveButton").on("click", function () {
     $("#display-saved").empty()
 
-    $("#display-saved").append($("<h1>").text("Saved Pallete"))
+    $("#display-saved").append($("<h1>").text("Saved"))
 
     for (i = 0; i < colorsArray.length; i++) {
         let color = colorsArray[i].color
@@ -9,8 +9,6 @@ $("#saveButton").on("click", function () {
         //reformat each color to css format -- rgb(12,50,39) 
         let rgbColor = `rgb(${color.red},${color.green},${color.blue})`
 
-        //displaying css formatted color in the DOM
-        $("#display-saved").append($("<div>").text(rgbColor).css(`background-color`, rgbColor).css('min-height', '10px'))
 
         //storing each color from the array into local storage
         localStorage.setItem(`savedColor${i}`, rgbColor)
